3b9a79b6f108ca39cee148efc757876660125fa1
SidHarder <softwarenavigator@gmail.com>
2017-07-13 15:57:09 -0400

    adding sid harder ic3 token

---
+++ sidharderic3token.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.0;
+
+contract coin {
+
+    string public constant name = "IC3 Token";
+    string public constant symbol = "IC3";
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply = 0;
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function  deposit() returns (bool _success) {
+        balances[msg.sender] += 1;
+        totalSupply += 1;
+        return true;
+    }
+
+    function withdraw(uint _amount) returns (bool _success) {
+        if(balances[msg.sender] >= _amount) {
+            balances[msg.sender] -= _amount;
+            totalSupply -= _amount;
+        } else { return false; }
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+}
