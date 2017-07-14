044c01d341c98314410529a6c1c45c61e5f6dfa7
Philip Daian <phil@linux.com>
2017-07-14 18:21:58 -0400

    fix reentrancy

--- reentrancy-warmup/vulnerable.sol
+++ reentrancy-warmup/vulnerable.sol
@@ -38,7 +38,7 @@ contract TestToken {
 
     function withdraw(uint256 _value) payable returns (bool success) {
         if (balances[msg.sender] < _value) return false;
-        msg.sender.call.value(_value)();
+        msg.sender.transfer(_value);
         balances[msg.sender] -= _value;
         total -= _value;
         return true;
